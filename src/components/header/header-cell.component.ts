3a32a4ee307cbba50848234af71eaec8d54177cd
a.abakumovs <a.abakumovs@intrum.com>
2016-12-19 14:53:30 +0200

    Pass directive context to make onSort usable

--- src/components/header/header-cell.component.ts
+++ src/components/header/header-cell.component.ts
@@ -29,7 +29,8 @@ import { nextSortDir } from '../../utils';
         [ngTemplateOutlet]="column.headerTemplate"
         [ngOutletContext]="{ 
           column: column, 
-          sortDir: sortDir
+          sortDir: sortDir,
+          directiveContext: this
         }">
       </template>
       <span
